81df78911ef9974b5ba052f93282d46c2eef7c78
Edward Gomez <edward@gomez.me.uk>
2019-09-04 14:01:18 +0100

    Add settings for day

--- camera.py
+++ camera.py
@@ -12,17 +12,21 @@ class Camera(BaseCamera):
     def frames():
         with picamera.PiCamera(resolution='1296x730', framerate=1) as camera:
             # let camera warm up
-            time.sleep(2)
-            if datetime.now().hour > 18 or datetime.now().hour < 6:
+            if datetime.now().hour > 18 or datetime.now().hour < 7:
                 camera.iso = 800
                 camera.exposure_mode = 'off'
                 camera.awb_mode = 'off'
                 camera.awb_gains = (Fraction(19, 16), Fraction(143, 128))
-                camera.vflip = True
-                camera.hflip = True
                 camera.shutter_speed = 800000
+            else:
+                camera.iso = 100
+                camera.exposure_mode = 'auto'
+                camera.awb_mode = 'auto'
+                camera.shutter_speed = 100000
             camera.vflip = True
             camera.hflip = True
+            time.sleep(2)
+
             stream = io.BytesIO()
             for _ in camera.capture_continuous(stream, 'jpeg',
                                                  use_video_port=True):
